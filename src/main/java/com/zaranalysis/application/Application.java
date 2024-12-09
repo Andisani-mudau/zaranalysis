28d119eb70fe2fabeaa232b78c7ed50507a04fde
start.vaadin.com <no-reply@vaadin.com>
2024-12-09 02:23:35 +0000

    Generated project

---
+++ src/main/java/com/zaranalysis/application/Application.java
@@ -0,0 +1,24 @@
+package com.zaranalysis.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/poppins", version = "4.5.0")
+@Theme(value = "zaranalysis")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
